11bda0cd2daacc7596b478591b1e403a41feb9b9
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-02-28 19:25:20 -0300

    feat: setup global styles with stitches and fix tsconfig base url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { styled } from "@/styles";
+
+export const Button = styled("button", {
+  backgroundColor: "$green500",
+  borderRadius: 4,
+  border: 0,
+  color: 'White',
+  padding: '4px 8px',
+});
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return <Button>Enviar</Button>;
 }
