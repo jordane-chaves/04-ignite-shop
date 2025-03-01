46758523d7c53ee766731a4a30324b1de747d4bd
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-02-28 22:14:44 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,96 @@
-import { styled } from '@/styles'
+import 'keen-slider/keen-slider.min.css'
 
-export const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  color: 'White',
-  padding: '4px 8px',
-})
+import { useKeenSlider } from 'keen-slider/react'
+import Image from 'next/image'
+
+import shirt1 from '@/assets/camisetas/1.png'
+import shirt2 from '@/assets/camisetas/2.png'
+import shirt3 from '@/assets/camisetas/3.png'
+import shirt4 from '@/assets/camisetas/4.png'
+import { HomeContainer, Product } from '@/styles/pages/home'
 
 export default function Home() {
-  return <Button>Enviar</Button>
+  const [sliderRef] = useKeenSlider({
+    mode: 'free-snap',
+    slides: {
+      perView: 1,
+      spacing: 48,
+      origin: 'center',
+    },
+    breakpoints: {
+      '(min-width: 600px)': {
+        slides: { perView: 1, spacing: 48, origin: 'center' },
+      },
+      '(min-width: 768px)': {
+        slides: { perView: 1.25, spacing: 48, origin: 'center' },
+      },
+      '(min-width: 1024px)': {
+        slides: { perView: 1.5, spacing: 48, origin: 0.1 },
+      },
+      '(min-width: 1200px)': {
+        slides: (sliderSize, slides) => {
+          const offset = (sliderSize - 1180) / 2
+          const perView = 1.5
+          const spacing = 48
+
+          return slides.map((_, index) => {
+            const size =
+              (sliderSize - (offset * 2 + spacing * (perView - 1))) / perView
+
+            let origin = offset
+
+            if (index > slides.length - perView) {
+              origin += (size + spacing) * (perView - (slides.length - index))
+            }
+
+            return {
+              origin: origin / sliderSize,
+              size: size / sliderSize,
+              spacing: spacing / sliderSize,
+            }
+          })
+        },
+      },
+    },
+  })
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} alt="" height={480} width={520} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} alt="" height={480} width={520} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" height={480} width={520} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={shirt4} alt="" height={480} width={520} />
+
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
