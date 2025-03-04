00356e5461eb3b8a7f6a54e0b3e42da45f5f5162
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-03-04 09:45:44 -0300

    feat: get products from stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import 'keen-slider/keen-slider.min.css'
 
 import { useKeenSlider } from 'keen-slider/react'
+import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Stripe from 'stripe'
 
-import shirt1 from '@/assets/camisetas/1.png'
-import shirt2 from '@/assets/camisetas/2.png'
-import shirt3 from '@/assets/camisetas/3.png'
-import shirt4 from '@/assets/camisetas/4.png'
+import { stripe } from '@/lib/stripe'
 import { HomeContainer, Product } from '@/styles/pages/home'
 
-export default function Home() {
+interface Product {
+  id: string
+  name: string
+  imageUrl: string
+  price: string
+}
+
+interface HomeProps {
+  products: Product[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     mode: 'free-snap',
     slides: {
@@ -56,41 +66,87 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={shirt1} alt="" height={480} width={520} />
+      {products.map((product) => (
+        <Product key={product.id} className="keen-slider__slide">
+          <Image src={product.imageUrl} alt="" height={480} width={520} />
 
-        <footer>
-          <strong>Camiseta Beyond the Limits</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt2} alt="" height={480} width={520} />
-
-        <footer>
-          <strong>Camiseta Beyond the Limits</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt3} alt="" height={480} width={520} />
-
-        <footer>
-          <strong>Camiseta Beyond the Limits</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={shirt4} alt="" height={480} width={520} />
-
-        <footer>
-          <strong>Camiseta Beyond the Limits</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
+      ))}
     </HomeContainer>
   )
 }
+
+/**
+ * **Busca os dados do lado do servidor (SSG)**
+ *
+ * Executa apenas durante o build da aplicação e quando o tempo do `revalidate`
+ * vencer e a próxima pessoa acessar a página. As próximas pessoas que acessarem
+ * vão acessar uma página em cache até vencer o tempo novamente.
+ *
+ * A vantagem é que a página estática irá carregar muito mais rápido e não é
+ * feito diversas requisições para a Stripe para buscar os dados.
+ *
+ * *OBS.: Não é possível ter acesso ao contexto da requisição.*
+ */
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products: Product[] = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: price.currency,
+      }).format(price.unit_amount / 100),
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  }
+}
+
+/**
+ * **Busca os dados do lado do servidor (SSR)**
+ *
+ * Executa em toda vez que a página é recarregada e a página só será mostrada
+ * em tela após ter executado essa função, ou seja, a página ficará "em branco"
+ * até ter carregado todos os dados.
+ *
+ * *OBS.: É possível ter acesso ao contexto da requisição.*
+ */
+/* export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products: Product[] = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+  }
+} */
